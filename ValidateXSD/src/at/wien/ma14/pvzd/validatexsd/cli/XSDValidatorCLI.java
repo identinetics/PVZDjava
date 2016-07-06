9de95455ad803cdd982a8be7e7103fb8db04f2ea
r2h2 <rainer@hoerbe.at>
2016-07-06 11:11:41 +0200

    move classloader dump to usage call (no args); UI cosmetics

--- ValidateXSD/src/at/wien/ma14/pvzd/validatexsd/cli/XSDValidatorCLI.java
+++ ValidateXSD/src/at/wien/ma14/pvzd/validatexsd/cli/XSDValidatorCLI.java
@@ -10,15 +10,10 @@ import java.net.URLClassLoader;
 public class XSDValidatorCLI {
 
     public void testVerifyGood(String xmldoc, String samlxsdDir) throws Exception {
-        ClassLoader cl = ClassLoader.getSystemClassLoader();
-        URL[] urls = ((URLClassLoader)cl).getURLs();
-        for(URL url: urls){
-            System.out.println(url.getFile());
-        }
         //String cwd_parent = (new File(new File(".").getAbsolutePath())).getPare0nt();
         XSDValidator validator = new XSDValidator(samlxsdDir, true);
-        validator.validateSchema(xmldoc);
-        System.out.println("done.");
+        String retmsg = validator.validateSchema(xmldoc);
+        System.out.println(retmsg);
     }
 
     public static void main(String[] argv) throws Exception {
@@ -26,14 +21,22 @@ public class XSDValidatorCLI {
         XSDValidatorCLI r = new XSDValidatorCLI();
         String default_xsd_dir = "/Users/admin/devl/java/rhoerbe/PVZDjava/ValidateXSD/SAML_MD_Schema";
         if (argv.length == 1) {
-            System.out.println("Validating " + argv[0] + "against xsd in " + default_xsd_dir);
+            System.out.println("using default xsd in " + default_xsd_dir);
             r.testVerifyGood(argv[0], default_xsd_dir);
         } else if (argv.length == 2) {
             System.out.println("Validating " + argv[0] + argv[1]);
             r.testVerifyGood(argv[0], argv[1]);
         } else {
             System.out.println("Usage: XSDValidatorCLI <file-to-be-validated> [schema-dir]");
+            System.out.println("Classpath by courtesy of Jhades");
             new JHades().printClasspath();
+            ClassLoader cl = ClassLoader.getSystemClassLoader();
+            System.out.println("Files loaded by SystemClassLoader");
+            URL[] urls = ((URLClassLoader)cl).getURLs();
+            for(URL url: urls){
+                System.out.println(url.getFile());
+            }
+
         }
     }
 
