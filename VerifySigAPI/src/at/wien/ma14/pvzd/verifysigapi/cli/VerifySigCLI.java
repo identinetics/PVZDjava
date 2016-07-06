b9080d968abe0fee1862565cd708ae0499c2d784
r2h2 <rainer@hoerbe.at>
2016-07-06 22:57:20 +0200

    added CLI

---
+++ VerifySigAPI/src/at/wien/ma14/pvzd/verifysigapi/cli/VerifySigCLI.java
@@ -0,0 +1,54 @@
+package at.wien.ma14.pvzd.verifysigapi.cli;
+
+import at.wien.ma14.pvzd.verifysigapi.PvzdVerifySig;
+import at.wien.ma14.pvzd.verifysigapi.PvzdVerifySigResponse;
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import static org.junit.Assert.assertEquals;
+import org.jhades.JHades;
+
+
+public class VerifySigCLI {
+    final File projdir = new File(System.getProperty("user.dir"));
+    final File moddir = new File(projdir, "VerifySigAPI");
+    //final File parentdir = new File(projdir.getParent());
+    final File log4jprop = new File(projdir, "conf/log4j_info.properties");
+    final File moaspprop = new File(projdir, "conf/moa-spss/MOASPSSConfiguration.xml");
+
+    public void testVerifyGood(String xmldoc) throws Exception {
+        File xmlFileOK = new File(moddir, xmldoc);
+        assert xmlFileOK.exists() : "not found: " + xmlFileOK.getAbsolutePath();
+        PvzdVerifySig verifier  = new PvzdVerifySig(moaspprop.getAbsolutePath(),
+                log4jprop.getAbsolutePath(),
+                xmlFileOK.getAbsolutePath());
+        PvzdVerifySigResponse response  = verifier.verify();
+        assertEquals("Expected OK for messageID", "OK", response.pvzdCode);
+        System.out.println("Signer certificate:" + response.signerCertificateEncoded);
+        System.out.println("Signed contents:");
+        for (String s : response.referencedata) {
+            System.out.println(s);
+        }
+    }
+
+    public static void main(String[] argv) throws Exception {
+        System.out.println("VerifySigCLI");
+        VerifySigCLI verifySigCLI = new VerifySigCLI();
+        if (argv.length == 1) {
+            System.out.println("Verifying signature");
+            verifySigCLI.testVerifyGood(argv[0]);
+        } else {
+            System.out.println("Usage: verifySigCLI <file-to-be-verified>");
+            System.out.println("Classpath by courtesy of Jhades");
+            new JHades().printClasspath();
+            ClassLoader cl = ClassLoader.getSystemClassLoader();
+            System.out.println("Files loaded by SystemClassLoader");
+            URL[] urls = ((URLClassLoader)cl).getURLs();
+            for(URL url: urls){
+                System.out.println(url.getFile());
+            }
+        }
+    }
+
+}
---
+++ VerifySigAPI/tests/VerifySigCLI.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# wrapper for VerifySigCLI
+# Usage: $0 <file-to-be-verified>
+
+ostype=${OSTYPE//[0-9.]/}
+if [[ "$ostype" == "linux-gnu" ]]; then
+    #  deployment env
+    export JAVA_HOME=/etc/alternatives/java_sdk_1.8.0
+    export PROJ_HOME=/opt/PVZDjava
+elif [[ "$ostype" == "darwin" ]]; then
+    if [[ `/bin/hostname` == "devl8.local" ]]; then  # r2h2 development env
+        export devlhome=~/devl
+        echo "I am @ devl8: ${devlhome}"
+    elif [[ `/bin/hostname` == "ClapTsuNami.local" ]]; then
+        export devlhome=/Volumes/devl
+        echo "I am ClapTsuNami"
+    else
+        echo "no environment defined for  host `/bin/hostname`"
+        exit 1
+    fi
+    export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home
+    export PROJ_HOME=$devlhome/java/rhoerbe/PVZDjava
+else
+    echo "no environment defined for $ostype"
+    exit 1
+fi
+
+export CLASSPATH=$PROJ_HOME/bin/test/ValidateXSD:$PROJ_HOME/bin/artifacts/pvzdVerifySig/pvzdVerifySig.jar:$PROJ_HOME/lib/unittests/jhades-1.0.4.jar
+
+$JAVA_HOME/bin/java at.wien.ma14.pvzd.verifysigapi.cli.VerifySigCLI $@
